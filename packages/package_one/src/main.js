e67bdf2429124dc262333ceb07e4c2a361836c9f
adowgail <adowgail@opentext.com>
2021-07-28 08:30:52 -0400

    initial

---
+++ packages/package_one/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+  // import * as pkgTwoStore from '@vue-workspace-test/package-two/src/store'
+
+Vue.config.productionTip = false
+
+// store.registerModule('pkgTwoStore', pkgTwoStore)
+
+new Vue({
+  store,
+  render: h => h(App)
+}).$mount('#app')
